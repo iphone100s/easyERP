bd226221878e8688c1d4afce861a9bd7c12624d0
win win <winwin780511@hotmail.com>
2020-01-16 14:48:12 +0800

    2020-1-14 登入功能新增密碼錯誤超過三次就鎖定 V1.12

--- easyERP/Login_Form.cs
+++ easyERP/Login_Form.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -29,15 +30,13 @@ namespace easyERP
             //設定標題的底色為透明標題
             label_title.BackColor = Color.Transparent;
             label_title.Parent = pictureBox_one;//將pictureBox設為標籤的父控件
-            //pictureBox_one.Controls.Add(label_title);
-          
-
+           //pictureBox_one.Controls.Add(label_title);
 
         }
 
         private void UserTextBox_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void userTextBox_Click(object sender, EventArgs e)
@@ -52,7 +51,7 @@ namespace easyERP
 
         private void Login_button_Click(object sender, EventArgs e)
         {
-            
+
             string username = this.userTextBox.Text;
             string password = this.passwordTextBox.Text;
 
@@ -63,153 +62,49 @@ namespace easyERP
             }
             else
             {
-                //到資料庫驗證
-                //string selectSql = "select * from tb_user where username='" + username + "'and password='" + password + "'"; //測試用資料庫
-
-                  string selectAccount = "select * from Member where memberAccount='" + username + "'and password='" + password + "'"; //正式用資料庫
-
-                //string selectAccount = "select memberAccount,password,permission from Member where memberAccount='" + username + "'and password='" + password + "'"; //正式用資料庫
-                //string selectAccount = "select memberAccount,password,permission from Member ";
-           
-
-
-                //StringBuilder sb = new StringBuilder();
-                //sb.Append(" select memberAccount,password,permission from Member where memberAccount='" + username + "'and password='" + password + "' ");
-
+               
+                string selectAccount = "select * from Member where memberAccount='" + username + "'and password='" + password + "'"; //正式用資料庫
 
                 SqlHelp sqlhelper = new SqlHelp();
 
-                // 1
-                //回傳為int 抓單筆資料 如果有就會為1
-                //int count = sqlhelper.SqlServerRecordCount(selectAccount); // 返回符合的結果數量
-                //if (count > 0)
-                //{
-                //    MessageBox.Show("資訊驗證成功 即將跳轉頁面");                   
-                //    Second_Form view = new Second_Form();//跳轉道主頁面
-                //    // Main view = new Main();//建立新的頁面
-                //    view.Show();//顯示新頁面
-                //    this.Hide();//隱藏當前頁面
-                //}
-                //else
-                //{
-                //    MessageBox.Show("使用者名稱或密碼錯誤");
-                //}
 
+                DataSet ds = sqlhelper.SqlServerRecordCount2(selectAccount); // 返回符合的結果數量     
 
-                // 2
-                //回傳為string 在SqlHelp此物件抓出字串並回到這判斷
-                //string ReturnPermission = sqlhelper.SqlServerRecordCount1(selectAccount); // 返回符合的結果數量
-                //if (ReturnPermission.Equals("A"))
-                //{
-                //    //if (ReturnPermission.Equals("A"))
-                //    //{
-                //    MessageBox.Show("資訊驗證成功 即將跳轉頁面");
-
-                //    //跳轉道主頁面
-                //    Second_Form view = new Second_Form();
-                //    // Main view = new Main();//建立新的頁面
-                //    view.Show();//顯示新頁面
-                //    this.Hide();//隱藏當前頁面
-                //                //break;
-                //                //}
-                //                //else
-                //                //{
-                //                //    MessageBox.Show("資訊驗證成功 但無權限");
-                //                //}
-                //}
-                //else
-                //{
-                //    MessageBox.Show("使用者名稱或密碼錯誤");
-                //}
-
-                // 3
-                //FOR迴圈抓資料(在這抓取物件資料 回傳字串)
-                //DataSet ds = sqlhelper.SqlServerRecordCount2(selectAccount); // 返回符合的結果數量
-                //string ReturnPermission = string.Empty;
-                //if (ds.Tables[0].Rows.Count > 0)
-                //{
-                //    for (int a = 0; a < ds.Tables[0].Rows.Count; a++)
-                //    {
-                //        ReturnPermission = ds.Tables["Member"].Rows[a]["permission"].ToString();
-
-                //        if (ReturnPermission.Equals("A"))
-                //        {
-                //            MessageBox.Show("資訊驗證成功 即將跳轉頁面");
-
-                //            //跳轉道主頁面
-                //            Second_Form view = new Second_Form();
-                //            // Main view = new Main();//建立新的頁面
-                //            view.Show();//顯示新頁面
-                //            this.Hide();//隱藏當前頁面
-                //            break;
-                //        }
-                //        else
-                //        {
-                //            MessageBox.Show("資訊驗證成功 但無權限");
-                //        }
-                //    }
-                //}
-                //else
-                //{
-                //    MessageBox.Show("使用者名稱或密碼錯誤");
-                //}
-
-                // 4
-                //單筆抓資料(在這抓取物件資料 回傳字串)
-                //DataSet ds = sqlhelper.SqlServerRecordCount2(selectAccount); // 返回符合的結果數量              
-                //if (ds.Tables[0].Rows.Count > 0)
-                //{
-                //    string ReturnPermission = ds.Tables["Member"].Rows[0]["permission"].ToString(); //.Rows[0] 抓一排 如果0改成1 就是往下抓一排
-
-                //    if (ReturnPermission.Equals("A"))
-                //    {
-                //        MessageBox.Show("資訊驗證成功 即將跳轉頁面");
-
-                //        //跳轉道主頁面
-                //        Second_Form view = new Second_Form();
-                //        // Main view = new Main();//建立新的頁面
-                //        view.Show();//顯示新頁面
-                //        this.Hide();//隱藏當前頁面
-
-                //    }
-                //    else
-                //    {
-                //        MessageBox.Show("資訊驗證成功 但無權限");
-                //    }
-                //}
-                //else
-                //{
-                //    MessageBox.Show("使用者名稱或密碼錯誤");
-                //}
-
-
-                DataSet ds = sqlhelper.SqlServerRecordCount2(selectAccount); // 返回符合的結果數量              
                 if (ds.Tables[0].Rows.Count > 0)
                 {
                     string ReturnPermission = ds.Tables["Member"].Rows[0]["permission"].ToString(); //.Rows[0] 抓一排 如果0改成1 就是往下抓一排
+                    int ReturnNemberError = (int)ds.Tables["Member"].Rows[0]["memberError"]; //.Rows[0] 抓一排 如果0改成1 就是往下抓一排
 
                     //string sqlname = ds.Tables["Member"].Rows[0]["memberName"].ToString();
-
+                    if (ReturnNemberError > 2)
+                    {
+                        MessageBox.Show("密碼已被鎖定!! 請洽管理員", "提示訊息", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        return;
+                    }
                     if (ReturnPermission.Equals("A") || ReturnPermission.Equals("B"))
                     {
-                         ReturnPermission = ds.Tables["Member"].Rows[0]["memberName"].ToString().Trim();
+                        ReturnPermission = ds.Tables["Member"].Rows[0]["memberName"].ToString().Trim();
 
-                        //MessageBox.Show( ReturnPermission + " 歡迎您登入，請按確定登入系統" );
+
+                        string updateDeleteMemberError = "UPDATE Member SET memberError = 0  WHERE memberAccount ='" + username + "' ; "; //正式用資料庫
+                        sqlhelper.SqlServerRecordCount2(updateDeleteMemberError);
+
 
                         MessageBox.Show(ReturnPermission + " 歡迎您登入，請按確定登入系統", "提示訊息", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                         //跳轉道主頁面
                         Second_Form view = new Second_Form();
-                        
+
                         view.Show();//顯示新頁面
                         this.Hide();//隱藏當前頁面
-                        //this.Close();//關閉視窗
-                        
+                                    //this.Close();//關閉視窗
+
                     }
                     if (ReturnPermission.Equals("C"))
                     {
+                        string updateDeleteMemberError = "UPDATE Member SET memberError = 0  WHERE memberAccount ='" + username + "' ; "; //正式用資料庫
+                        sqlhelper.SqlServerRecordCount2(updateDeleteMemberError);
 
-                        //MessageBox.Show( ReturnPermission + " 歡迎您登入，請按確定登入系統");
                         MessageBox.Show(ReturnPermission + " 歡迎您登入，請按確定登入系統", "提示訊息", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                         //跳轉道主頁面
@@ -222,12 +117,15 @@ namespace easyERP
                     }
 
                 }
-                else
+                if (ds.Tables[0].Rows.Count == 0)
                 {
-                    MessageBox.Show("使用者名稱或密碼錯誤", "提示訊息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    string updateAddMemberError = "UPDATE Member SET memberError = memberError + 1 WHERE memberAccount ='" + username + "' ; "; //正式用資料庫
+
+                    sqlhelper.SqlServerRecordCount2(updateAddMemberError);
+
+                    MessageBox.Show("帳號或密碼錯誤喔!!", "提示訊息", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
                 }
-
-
 
             }
         }
